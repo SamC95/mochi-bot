630c22d5b268c9d58df629250904e9f42248d4c6
SamC95 <132593571+sJagstang@users.noreply.github.com>
2024-12-12 00:25:32 +0000

    fix: Changed update handler condition to OR instead of AND.

--- src/main/java/com/example/mochibot/utils/firestore/UpdateHandler.java
+++ src/main/java/com/example/mochibot/utils/firestore/UpdateHandler.java
@@ -18,14 +18,14 @@ public class UpdateHandler {
       String currentTitle = docSnapshot.getString("title");
       String currentUrl = docSnapshot.getString("url");
 
+      boolean isSameTitle = Objects.equals(currentTitle, post.getTitle());
+      boolean isSameUrl = Objects.equals(currentUrl, post.getUrl());
+
       /*
       Both title and url are checked to avoid scenarios where the title may be updated due to typos or other reasons,
       which would cause a repost if it no longer matches the currently stored one.
       */
-      boolean isSameTitle = Objects.equals(currentTitle, post.getTitle());
-      boolean isSameUrl = Objects.equals(currentUrl, post.getUrl());
-
-      if (isSameTitle && isSameUrl) {
+      if (isSameTitle || isSameUrl) {
         System.out.printf("[%s] [INFO] No new post for %s\n", LocalTime.now(), documentName);
         return null;
       }
